754eb8c1f4505d4fa2d6d2a764823bb73bcad880
yuanlq <yuanlq@pku.edu.cn>
2021-10-03 00:05:09 +0800

    init

---
+++ HW1/homework/fig/main.m
@@ -0,0 +1,61 @@
+%
+%  main.m
+%  wm2
+%
+%  Created by Yuan Leiqi (袁磊祺) on 2021/10/1.
+%
+
+clear;clc;close all;
+set(0, 'defaultlinelinewidth', 1)
+set(0, 'defaultaxeslinewidth', 2);
+set(0, 'defaultaxesfontsize', 28);
+set(0, 'defaulttextfontsize', 28);
+set(0, 'DefaultLineMarkerSize', 10);
+set(0, 'Defaultaxesfontname', 'Times New Roman');
+
+a = 0.5;
+b = 7;
+a * b - 1 - 3/2 * pi
+iter = 15;
+long = 32 / b;
+n = 1e5;
+randLenth = 0.003;
+randN = floor(randLenth * n / long);
+x = linspace(0, long, n);
+y = 0;
+
+for i = 1:iter
+    y = a^i * cos(b^i * pi * x) + y;
+end
+
+loca = 1;
+a1 = 0.1;
+
+for i = 1:n
+    yNew(i) = y(loca);
+    loca = loca + randi([-randN randN], 1, 1);
+    loca = mod(loca, n);
+
+    if (loca == 0)
+        loca = 1;
+    end
+
+end
+
+y = yNew;
+
+stru = 0;
+
+for j = 1:n - 1
+    squarAll = 0;
+
+    for i = 1:n - j
+        squarAll = squarAll + (y(i + j) - y(i))^2;
+    end
+
+    stru(j) = squarAll / (n - j);
+end
+
+save(strcat(num2str(n), 'x.mat'), 'x')
+save(strcat(num2str(n), 'y.mat'), 'y')
+save(strcat(num2str(n), 'stru.mat'), 'stru')
