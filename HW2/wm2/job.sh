9ae631ec571ead2b3586e127b13a12da63350fb8
yuanlq <yuanlq@pku.edu.cn>
2021-12-22 16:01:12 +0800

    HW4 begins

--- HW2/wm2/job.sh
+++ HW2/wm2/job.sh
@@ -2,11 +2,10 @@
 #SBATCH -o slurm.%j.out
 #SBATCH -p compute
 #SBATCH --qos=normal
-#SBATCH -J lqy
+#SBATCH -J yyc
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --mail-type=end
-#SBATCH --mail-user=circle20101561@163.com
 
 
 module load matlab/R2021a
